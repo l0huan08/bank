58d79db88dc45ac8bd9e25644ab8652ba554661e
huangli <l0huan08@gmail.com>
2014-09-18 11:05:02 -0400

    add Admin entity class

--- Bank/src/bank/entity/AccountType.java
+++ Bank/src/bank/entity/AccountType.java
@@ -1,5 +1,21 @@
 package bank.entity;
 
 public class AccountType {
-
+	private int typeId=0;
+	private String typeName=null;
+	
+	public int getTypeId() {
+		return typeId;
+	}
+	public void setTypeId(int typeId) {
+		this.typeId = typeId;
+	}
+	public String getTypeName() {
+		return typeName;
+	}
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+	
+	
 }
--- Bank/src/bank/entity/Client.java
+++ Bank/src/bank/entity/Client.java
@@ -1,5 +1,98 @@
 package bank.entity;
 
-public class Client {
+import java.util.Date;
 
+public class Client {
+	private int clientId=0;
+	private String firstName=null;
+	private String middleName=null;
+	private String lastName=null;
+	private String gender=null; //M,F
+	private Date birthday=null;
+	private String tel=null;
+	private String add1=null;
+	private String add2=null;
+	private String zip=null; //221008
+	private String email=null;
+	private String username=null;
+	private String password=null;
+	
+	public int getClientId() {
+		return clientId;
+	}
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAdd1() {
+		return add1;
+	}
+	public void setAdd1(String add1) {
+		this.add1 = add1;
+	}
+	public String getAdd2() {
+		return add2;
+	}
+	public void setAdd2(String add2) {
+		this.add2 = add2;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
