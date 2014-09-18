0f6b9ecd0bb07e036440fb2cdd3fb0d783636ef8
runyan <yanrun2007@gmail.com>
2014-09-18 12:46:23 -0400

    minor changes

---
+++ Bank/src/bank/servlet/LogoutServlet.java
@@ -0,0 +1,31 @@
+package bank.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String username = (String)session.getAttribute("");
+		if(username != null){
+			session.removeAttribute("");
+			response.sendRedirect("");
+		}
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
