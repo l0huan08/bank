4ef23d1a379184c2aca8bd063241f6af7ac529b1
runyan <yanrun2007@gmail.com>
2014-09-24 10:43:35 -0400

    change the configuration of the filters

--- Bank/src/filter/AuthorityFilter.java
+++ Bank/src/filter/AuthorityFilter.java
@@ -8,10 +8,12 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebFilter(description = "right", urlPatterns = { "*.jsp" })
 public class AuthorityFilter  implements Filter{
 
 	@Override
